756ab9f37c65fc547d6e8068e53bc91a751eb5ca
Vu Kim Duy <duyvukim752000@gmail.com>
2021-03-29 19:58:37 +1100

    [ADDED] error handling for database connection

--- private/database/db.php
+++ private/database/db.php
@@ -5,6 +5,7 @@ require_once("db_credential.php");
 function db_connect()
 {
     $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+    db_confirm_connection();
     return $connection;
 }
 
@@ -14,5 +15,24 @@ function db_disconnect($link)
 {
     if (isset($link)) {
         mysqli_close($link);
+    }
+}
+
+
+/** Check database connectio */
+function db_confirm_connection()
+{
+    if (mysqli_connect_errno()) {
+        $msg  = "Database connection failed: ";
+        $msg .= mysqli_connect_error() . ". Code: " . mysqli_connect_errno();
+        exit($msg);
+    }
+}
+
+/** Check the returned result from the query */
+function db_confirm_query($result)
+{
+    if (!$result) {
+        exit("Cannot retrieve back the result from database. Please check the query string or database connection again!");
     }
 }
--- private/database/db_query_functions.php
+++ private/database/db_query_functions.php
@@ -5,6 +5,7 @@ function query_all_values_order_by($table, $orderby)
 {
     global $db;
     $query = "SELECT * FROM $table ORDER BY $orderby desc";
-    $returnedValues = mysqli_query($db, $query);
-    return $returnedValues;
+    $result = mysqli_query($db, $query);
+    db_confirm_query($result);
+    return $result;
 }
