a0b51ce5356d7127c4fa3507bfa89d3cc9991bde
Vu Kim Duy <duyvukim752000@gmail.com>
2021-03-30 13:37:56 +1100

    [MODIFIED] create_new_page.php, making some feeatures, adding some data-driven

--- public/staff/pages/create_page.php
+++ public/staff/pages/create_page.php
@@ -1,42 +1,67 @@
 <?php
 require_once("../../../private/initialize.php");
-$page_title = "Create New Page";
 
-$page_name  = "";
-$active     = "";
+$menu_name  = "";
+$position   = "";
+$visible    = "";
+$content    = "";
 
 if (is_request("post")) {
-    $page_name  = $_POST["menu_name"] ?? "";
-    $position     = $_POST[""] ?? "";
+    $menu_name    = $_POST["menu_name"] ?? "";
+    $position     = $_POST["position"] ?? "";
+    $visible      = $_POST["visible"] ?? "";
 
-
-    echo "Form Submitted </br>";
-    echo "Page Name: " . $page_name . "</br>";
-    echo "Is Active: " . $active . "</br>";
+    echo "Form parameters<br />";
+    echo "Menu name: " . $menu_name . "<br />";
+    echo "Position: " . $position . "<br />";
+    echo "Visible: " . $visible . "<br />";
 }
 
-
 ?>
+<?php $page_title = "Create New Page"; ?>
 <?php include(SHARED_PATH . "/staff_header.php"); ?>
 
 <!-- HTMl -->
 <div id="content">
-    <a class="back-link" href="">&#171 Back to Staff Area</a>
+    <a class="back-link" href="<?php echo url_for("/staff/pages/index.php") ?>">&#171 Back to Staff Area</a>
 
     <div class="subject new">
         <h1>Create page</h1>
         <form action="<?php echo url_for("/staff/pages/create_page.php"); ?>" method="post">
             <dl>
                 <dt>Page Name</dt>
-                <dd><input type="text" name="page_name" value="<?php echo $page_name ?>"></dd>
+                <dd><input type="text" name="menu_name" value="<?php echo secure_http($menu_name); ?>" /></dd>
             </dl>
             <dl>
-                <dt>is Active</dt>
+                <dt>Position</dt>
                 <dd>
-                    <select name="is_active">
-                        <option value="1">Active</option>
-                        <option value="0">In-active</option>
+                    <select name="position">
+                        <option value="1" <?php if ($position == 1) {
+                                                echo "selected";
+                                            } ?>>1</option>
+                        <option value="2" <?php if ($position == 2) {
+                                                echo "selected";
+                                            } ?>>2</option>
+                        <option value="3" <?php if ($position == 3) {
+                                                echo "selected";
+                                            } ?>>3</option>
                     </select>
+                </dd>
+            </dl>
+            <dl>
+                <dt>Visible</dt>
+                <dd>
+                    <input type="hidden" name="visible" value="0">
+                    <input type="checkbox" name="visible" value="1" <?php if ($visible == "1") {
+                                                                        echo " checked";
+                                                                    } ?>>
+                </dd>
+            </dl>
+            <dl>
+                <dt>Content</dt>
+                <dd>
+                    <textarea name="content" rows="4" cols="13" placeholder="Type the content in here..."
+                        maxlength="100" required></textarea>
                 </dd>
             </dl>
             <div id="operations">
